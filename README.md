3ab18319f1c48c80d50a57b9f8135c786490f385
Trevor Johnson <trevor9j@protonmail.com>
2024-09-23 14:21:00 -0700

    finished the project functionality, and the ability to store current round and score via localStorage. Added ReadMe.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Number Guesser Game
+
+This project is Codecademy's "Challenge Project: Number Guesser" project. The goal of this project is to create a guessing game where the user plays against the computer. A random number ('target number') is generated within the JavaScript code, and compared against the computer's number guess and the users guess. The computers number is randomly generated ('computer guess') within the JavaScript code, and the user's number is manually entered into the input field on the users screen.
+
+I've solved it via their website, but wanted to practice it again, and expand my learning. Ive done so by adding a layer of sophistication by applying to it the functionality of storing, and retrieving scores from the local storage within the browser. 
+
+The script.js file contains the code that Ive written for this project. While the index.html and game.js files were code that I've downloaded from Codecademy.com. The downloaded code is everything that is needed in order for me to focus on the current level of learning that I am on in this program. AKA building the code within the script.js file.
+
+I would also like to add the capability to reset the scores and rounds.
+
+## Installation
+No need to install anything. Simply open the links below to play the game directly.
--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 let humanScore = localStorage.getItem('humanScore') ? parseInt(localStorage.getItem('humanScore')) : 0;
 let computerScore = localStorage.getItem('computerScore') ? parseInt(localStorage.getItem('computerScore')) : 0;
-let currentRoundNumber = 1;
+let currentRoundNumber = localStorage.getItem('currentRoundNumber') ? parseInt(localStorage.getItem('currentRoundNumber')) : 0;
 
-let humanGuess = 3;
-let computerGuess = 8;
+let humanGuess = 0;
+let computerGuess = 0;
+
 // Write your code below:
-//Task I
+/*Task I: creating a random number that we will use to weigh the computer and human guess against.*/
 const generateTarget = () => {
   const ranNum = Math.floor(Math.random() * 10);
   return ranNum;
 }
 console.log(generateTarget());
 
-//Task II
+//Task II: 
+/* targetNum is assigned the generateTarget() function outside of the compareGuesses funtion. THis maintains consistency within the program, rather assinging targetNum this value inside the compareGuesses function. */
+const targetNum = generateTarget();
+
+/* Based on the manual human guess and the automated computer guess, here we compare the differences in values next to the automated targetNum. If human guess is closest or equal to targetNum, the boolean value true is returned. Else, false is returned. */
 const compareGuesses = (humanGuess, computerGuess, targetNum) => {
-  targetNum = generateTarget();
-  if (humanGuess === computerGuess) {
-    return `It's a tie! `;
-} else if (Math.abs(targetNum - computerGuess) > Math.abs(targetNum - humanGuess)) {
-  return true;
-} else {
-  return false;
-  }
+  const humanDiff = Math.abs(humanGuess - computerGuess);
+  const computerDiff = Math.abs(targetNum - computerGuess);
+  return humanDiff <= computerDiff;
 }
+//Calling the function for testing purposes
+console.log(compareGuesses(humanGuess, computerGuess, targetNum));
 
-console.log(compareGuesses(humanGuess, computerGuess));
-
-//Task III
-const updateScore = (winner) => {
+/* Task III : This updates the computer or humans score depending on who wins the current round */
+const updateScore = winner => {
   if (winner === 'computer') {
     computerScore++;
     localStorage.setItem('computerScore', computerScore);
-  } else {
+  } else if (winner === 'human'){
     humanScore++;
     localStorage.setItem('humanScore', humanScore);
   }
 }
-updateScore('computer');
-console.log(computerScore);
+/* Task IV:*/ 
+const advancedRound = (winner) => {
+  currentRoundNumber++;
+  localStorage.setItem('currentRoundNumber', currentRoundNumber)
+}
+
 
